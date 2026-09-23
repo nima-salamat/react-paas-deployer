0aa935d674ff31deaf291fa78b4ea0cc2368ff33
nima <152262921+nima-salamat@users.noreply.github.com>
2026-09-23 07:47:44 +0330

    refactor messenger: route shell dependencies through feature APIs

--- src/components/messenger/MessengerApp.jsx
+++ src/components/messenger/MessengerApp.jsx
@@ -64,15 +64,15 @@ import { ImageCropDialog, ReadReceiptsDialog, MessengerDialogs, MessageSearchDia
 import { AudioPlayerBar, MediaGalleryDialog, ChatMediaLibraryDialog, VideoEditDialog, PreviewTextBody } from "./features/media";
 import { MessengerProfileEditor } from "./features/profile";
 import { JitsiCallModal, IncomingCallBanner, useMessengerCalls, useMessengerWebSocket } from "./features/calls";
-import { attachMessengerOriginal, messengerOriginalOf, attachMessengerImageEdits, messengerImageEditsOf, attachMessengerVideoEdits, messengerVideoEditsOf, finalizeMessengerFiles, guessLangFromName } from "./modules/fileHelpers";
-import { mergeConversations } from "./modules/mergeConversations";
+import { attachMessengerOriginal, messengerOriginalOf, attachMessengerImageEdits, messengerImageEditsOf, attachMessengerVideoEdits, messengerVideoEditsOf, finalizeMessengerFiles, guessLangFromName } from "./features/media";
+import { mergeConversations } from "./features/inbox";
 import {
   writeComposerDraft,
   readComposerDraft,
   resolveComposerDraft,
   draftPayload,
-} from "./modules/composerDrafts";
-import { isGroupDescDismissed, persistGroupDescDismiss } from "./modules/groupDescDismiss";
+} from "./features/composer";
+import { isGroupDescDismissed, persistGroupDescDismiss } from "./features/dialogs";
 import useKeyboardLayout from "./hooks/useKeyboardLayout";
 
 
@@ -92,11 +92,11 @@ import {
   writeMessengerMsgCache,
   touchMessengerMsgCache,
   MSG_SESSION_MAX_MSGS,
-} from "./modules/msgCache";
-import { parseCallSystemBody, formatCallSystemLabel, normalizeMessage, normalizeMessages } from "./modules/callSystemMessage";
-import { readAppearance, writeAppearance, getPalette, normalizeColorThemeId } from "./modules/appearance";
-import { getScrollPrefetchPlan, shouldChainLoadOlder, shouldChainLoadNewer } from "./modules/scrollPrefetch";
-import { MSG_SCROLL_STYLE_TEXT } from "./modules/msgScrollStyles";
+} from "./features/messages";
+import { parseCallSystemBody, formatCallSystemLabel, normalizeMessage, normalizeMessages } from "./features/calls";
+import { readAppearance, writeAppearance, getPalette, normalizeColorThemeId } from "./features/settings";
+import { getScrollPrefetchPlan, shouldChainLoadOlder, shouldChainLoadNewer } from "./features/messages";
+import { MSG_SCROLL_STYLE_TEXT } from "./features/messages";
 
 export default function MessengerApp({ themeMode = "system", onThemeModeChange }) {
   const theme = useTheme();
