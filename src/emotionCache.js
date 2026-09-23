032dcd00fd12e252459f5b90f46e01575d357900
nima <152262921+nima-salamat@users.noreply.github.com>
2026-09-23 07:03:23 +0330

    fix ssr: use a DOM-free Emotion cache on prerender

--- src/emotionCache.js
+++ src/emotionCache.js
@@ -1,7 +1,27 @@
 import createCache from "@emotion/cache";
 
+const createServerCache = () => ({
+  key: "pd",
+  registered: Object.create(null),
+  inserted: Object.create(null),
+  compat: true,
+  nonce: undefined,
+  sheet: {
+    tags: [],
+    insert: () => {},
+    flush: () => {},
+    hydrate: () => {},
+  },
+  insert: (_selector, serialized) => serialized?.styles || "",
+});
+
 export function createEmotionCache() {
+  if (typeof document === "undefined") {
+    return createServerCache();
+  }
+
   return createCache({
     key: "pd",
+    prepend: true,
   });
 }
