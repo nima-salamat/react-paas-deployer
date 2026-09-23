ddc5390bd9f7c4d7ca328ec0100b801874e2260e
nima <152262921+nima-salamat@users.noreply.github.com>
2026-09-23 06:59:04 +0330

    fix ssr: provide a stable Emotion cache for prerendering

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 
 import { ProfileProvider } from "./components/profile/profile.jsx";
 import App from "./App.jsx";
 import CustomCursor from "./components/layout/CustomCursor.jsx";
 
-export default function Root() {
+export function createEmotionCache() {
+  return createCache({
+    key: "pd",
+    prepend: true,
+  });
+}
+
+export default function Root({ emotionCache = null }) {
+  const cache = emotionCache || createEmotionCache();
+
   return (
-    <ProfileProvider>
-      <CustomCursor />
-      <App />
-    </ProfileProvider>
+    <CacheProvider value={cache}>
+      <ProfileProvider>
+        <CustomCursor />
+        <App />
+      </ProfileProvider>
+    </CacheProvider>
   );
 }
