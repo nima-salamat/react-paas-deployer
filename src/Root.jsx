88bf8e618f7706ac208dd5706a84cc8d19fe8d8e
nima <152262921+nima-salamat@users.noreply.github.com>
2026-09-23 07:01:08 +0330

    refactor ssr: reuse the shared Emotion cache factory

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import { CacheProvider } from "@emotion/react";
-import createCache from "@emotion/cache";
+import { createEmotionCache } from "./emotionCache";
 
 import { ProfileProvider } from "./components/profile/profile.jsx";
 import App from "./App.jsx";
 import CustomCursor from "./components/layout/CustomCursor.jsx";
-
-export function createEmotionCache() {
-  return createCache({
-    key: "pd",
-    prepend: true,
-  });
-}
 
 export default function Root({ emotionCache = null }) {
   const cache = emotionCache || createEmotionCache();
