805623e6f6fe06e5b4b0dc4f4867409161d5f1b5
nima <152262921+nima-salamat@users.noreply.github.com>
2026-09-23 07:48:17 +0330

    refactor messenger: move generic context menu into shared feature API

--- src/components/messenger/features/shared/index.js
+++ src/components/messenger/features/shared/index.js
@@ -1,5 +1,3 @@
 export { default as ContextMenu } from "../../components/ContextMenu";
-export { default as AudioPlayerBar } from "../../components/AudioPlayerBar";
-export { default as PreviewTextBody } from "../../components/PreviewTextBody";
 export * from "../../messengerUtils";
 export * from "../../api";
